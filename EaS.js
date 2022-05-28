a84155843405279f0778f4932cadb2cf4703e78c
Kanishk Mishra <Kanishkmishra3528@gmail.com>
2022-05-28 12:08:14 -0400

    Grid and highlight complete, working on resizing using JS

--- EaS.js
+++ EaS.js
@@ -1,11 +1,46 @@
-// create grid squares
-const container = document.querySelector('.container');
+// create grid
+function createGrid(size = 16) {
+    const container = document.querySelector('.container');
 
-for(let i = 0; i<(16**2); i++) {
-    let box = document.createElement('div');
+    // remove previous grid
+    while(container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    }
 
-    box.classList.add('box');
-    box.style.border = "5px solid black";
+    // create new grid squares
+    for(let i = 0; i<(size**2); i++) {
+        let box = document.createElement('div');
 
-    container.appendChild(box);
+        box.classList.add('box');
+        box.style.border = "1px solid black";
+
+        container.appendChild(box);
+    }
+
+    container.style.gridTemplateColumns = "repeat(size, 1fr)";
+
+    // establish hover event
+    const grid = document.querySelectorAll('.box');
+
+    grid.forEach(pixel => {
+        pixel.addEventListener('mouseenter', ()=>{
+            pixel.classList.add('hover');
+        });
+    });
 }
+
+createGrid();
+
+// resize grid
+const resize = document.querySelector('button');
+
+resize.addEventListener('click', ()=>{
+    let size = window.prompt("What width/length would you like? (max 100)");
+    
+    //ensure valid input
+    while ((size<1) || (size>100)) {
+        size = window.prompt("Please input a valid width/length? (max 100)");
+    }
+
+    createGrid(size);
+});
